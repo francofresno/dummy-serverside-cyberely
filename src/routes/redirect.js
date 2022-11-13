91719a41269465386a0d556dfdcc58135963510c
franco fresno <franco.fresno@gmail.com>
2022-11-13 20:21:23 -0300

    fix open redirect

--- src/routes/redirect.js
+++ src/routes/redirect.js
@@ -2,14 +2,26 @@ const express = require("express");
 const path = require("path");
 const router = express.Router();
 
+const whitelist = ["https://forms.gle/m1isNQdp6kX4MxY17"];
+
 router.get("/", async function (req, res) {
   const origin = req.get("origin");
   res.set("Access-Control-Allow-Origin", origin);
   const { dest } = req.query;
-  res.location(dest);
 
+  const estaEnLaList = whitelist.some((p) => p === dest);
+  if (dest && estaEnLaList) {
+    res.location(dest);
+    res.set("Content-Type", "text/html");
+    res.status(302).sendFile(path.join(__dirname + "/pages/redirect.html"));
+    return;
+  }
   res.set("Content-Type", "text/html");
-  res.status(302).sendFile(path.join(__dirname + "/pages/redirect.html"));
+  res.status(401).sendFile(path.join(__dirname + "/pages/noAutorizada.html"));
+
+  // res.location(dest);
+  // res.set("Content-Type", "text/html");
+  // res.status(302).sendFile(path.join(__dirname + "/pages/redirect.html"));
 });
 
 module.exports = router;
