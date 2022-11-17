6ad45355b19bfee0b8d68d186ffc9133ecc4cd56
franco fresno <franco.fresno@gmail.com>
2022-11-17 01:46:02 -0300

    port2

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const admin = require("./src/routes/admin");
 const redirect = require("./src/routes/redirect");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
